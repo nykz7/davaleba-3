718c10d3dd158571a5e5b65ca2dd830ab935afa9
niika <niika@NK>
2025-06-16 03:22:19 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿//Console.WriteLine(1 == 1);
+//Console.WriteLine(2 == 1);
+//Console.WriteLine(1 + 1);
+//Console.WriteLine(2 - 1);
+//Console.WriteLine(4 / 2);
+//Console.WriteLine(5 * 5);
+
+//Console.WriteLine("Enter a first number");
+//byte numFirst = byte.Parse(Console.ReadLine());
+//Console.WriteLine("Enter a second number");
+//byte numSecond = byte.Parse(Console.ReadLine());
+
+//Console.WriteLine($"Your first number is {numFirst} and your second number is {numSecond}");
+//Console.WriteLine(numFirst + numSecond);
+//Console.WriteLine(numFirst - numSecond);
+//Console.WriteLine(numFirst / numSecond);
+//Console.WriteLine(numFirst * numSecond);
+//Console.WriteLine(numFirst == numSecond);
+
+//int x = 2;
+//Console.WriteLine(x *= 3);
+//Console.WriteLine(x /= 4);
+
+//Console.WriteLine("Enter first number:");
+//byte numberFirst = byte.Parse(Console.ReadLine());
+//Console.WriteLine("Enter second number:");
+//byte secondNumber = byte.Parse(Console.ReadLine());
+
+//if (numberFirst == secondNumber)
+//{
+//    Console.WriteLine("es ricxvebi tolia");
+//} else if (numberFirst > secondNumber)
+//{
+//    Console.WriteLine("pirveli ricxvi metia meoreze");
+//} else if (numberFirst < secondNumber)
+//{
+//    Console.WriteLine("meore ricxvi metia pirvelze");
+//}
+
+//Console.WriteLine("Enter number:");
+//byte number = byte.Parse(Console.ReadLine());
+
+//if (number > 5 && number < 10)
+//{
+//    Console.WriteLine("Ricxvi shualedshia");
+//} else
+//{
+//    Console.WriteLine("Wrong number");
+//}
+
+Console.WriteLine("Enter number:");
+byte ricxvi = byte.Parse(Console.ReadLine());
+
+if (ricxvi == 5 || ricxvi == 10)
+{
+    Console.WriteLine("Right number");
+} else
+{
+    Console.WriteLine("Wrong number");
+}
+
