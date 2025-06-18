28a81aff9e878a20e2417c03ce2fb37c85469e80
nykz7 <niikanar2904@gmail.com>
2025-06-18 17:57:14 +0400

    Davaleba validacia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿//Console.WriteLine(1 == 1);
+﻿
+#region
+//Console.WriteLine(1 == 1);
 //Console.WriteLine(2 == 1);
 //Console.WriteLine(1 + 1);
 //Console.WriteLine(2 - 1);
@@ -48,14 +50,86 @@
 //    Console.WriteLine("Wrong number");
 //}
 
-Console.WriteLine("Enter number:");
-byte ricxvi = byte.Parse(Console.ReadLine());
+//Console.WriteLine("Enter number:");
+//byte ricxvi = byte.Parse(Console.ReadLine());
 
-if (ricxvi == 5 || ricxvi == 10)
+//if (ricxvi == 5 || ricxvi == 10)
+//{
+//    Console.WriteLine("Right number");
+//} else
+//{
+//    Console.WriteLine("Wrong number");
+//}
+
+#endregion
+
+#region
+
+//Console.Write("First Number: ");
+//int firstNumber = int.Parse(Console.ReadLine());
+
+//Console.Write("Second Number: ");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//Console.Write("[+ - * /]");
+//char operation = char.Parse(Console.ReadLine());
+
+
+//switch (operation)
+//{
+//    case '+':
+//        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+//        break;
+//    case '-':
+//        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+//        break;
+//    case '*':
+//        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+//        break;
+//    case '/':
+//        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+//        break;
+//    default:
+//        break;
+//}
+
+
+Console.WriteLine("Enter your email:");
+string email = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(email) || !email.Contains("@") || !email.Contains(".") || email.StartsWith("@") || email.EndsWith("@"))
 {
-    Console.WriteLine("Right number");
+    Console.WriteLine("Wrong input");
+    return;
 } else
 {
-    Console.WriteLine("Wrong number");
+    Console.WriteLine("valid email");
 }
 
+Console.WriteLine("Enter your password:");
+string password = Console.ReadLine();
+
+if (!password.Any(char.IsUpper))
+{
+    Console.WriteLine("Password must contain at least one upper letter");
+    return;
+}
+else if (!password.Any(ch => "!@#$%^&*()_+{}[]:;\"'<>,.?/\\|".Contains(ch)))
+{
+    Console.WriteLine("password must contain at least one symbol");
+    return;
+}
+else if (!password.Any(char.IsDigit))
+{
+    Console.WriteLine("Passwored must contain at least one number");
+}
+else if (password.Length < 8)
+{
+    Console.WriteLine("password must be at least 8 character");
+}
+else
+{
+    Console.WriteLine("Password is valid!");
+}
+
+#endregion
